005972d12bab72b4b6c5d46edd503fbccf8f83ca
davinci <simpladev@yandex.ru>
2019-10-29 00:03:58 +0300

    stage 291019-0033

--- app/Entity/Shop/Catalog/Product/ProductObserver.php
+++ app/Entity/Shop/Catalog/Product/ProductObserver.php
@@ -4,6 +4,9 @@ namespace App\Entity\Shop\Catalog\Product;
 
 use Illuminate\Support\Str;
 use App\Entity\Shop\Product\Product;
+use App\Entity\Shop\Product\Variant;
+use App\Entity\Shop\Product\Attribute;
+use App\Entity\Shop\Product\Image\Image;
 
 class ProductObserver
 {
@@ -15,5 +18,23 @@ class ProductObserver
         if ($product->slug == '') {
             $product->slug = Str::slug($product->name);
         }
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function deleting(Product $product)
+    {
+        $product->images->map(function (Image $image) {
+            $image->delete();
+        });
+
+        $product->variants->map(function (Variant $variant) {
+            $variant->delete();
+        });
+
+        $product->attributes->map(function (Attribute $attribute) {
+            $attribute->delete();
+        });
     }
 }
