5a6f477e2219091ef206334abce8b0011258aae3
Balyhin <balyhin@tpcorp.ru>
2019-11-08 10:41:34 +0300

    stage 0871119-1041

--- app/Entity/Shop/Catalog/Product/Scopes.php
+++ app/Entity/Shop/Catalog/Product/Scopes.php
@@ -37,34 +37,17 @@ trait Scopes
      */
     public function scopeWhereCategoryIdsAndDescendants(Builder $query, array $ids)
     {
-        $query->select('products.*');
+        $nestedSet = function ($query) use ($ids) {
+            $query
+                ->fromRaw('categories nested_set_0, categories nested_set_1')
+                ->selectRaw('distinct nested_set_0.id')
+                ->whereIn('nested_set_1.id', $ids)
+                ->whereRaw('nested_set_0._lft between nested_set_1._lft and nested_set_1._rgt');
+        };
 
-        $query->join(Tables::PRODUCT_CATEGORIES . ' as pc', function (JoinClause $join) use ($ids) {
-            $join->on('pc.product_id', 'id');
+        $query->join(Tables::PRODUCT_CATEGORIES . ' as pc', function (JoinClause $join) use ($nestedSet) {
+            $join->on('pc.product_id', 'id')->whereIn('pc.category_id', $nestedSet);
         });
-
-        $query->join('categories as nested_set_0', function (JoinClause $join) {
-            $join->on('nested_set_0.id', 'pc.category_id');
-        });
-
-        $query->join('categories as nested_set_1', function (JoinClause $join) use ($ids) {
-            $join->whereIn('nested_set_1.id', $ids);
-            $join->whereRaw('nested_set_0._lft between nested_set_1._lft and nested_set_1._rgt');
-        });
-
-        $query->groupBy('products.id');
-
-//                $nestedSet = function ($query) use ($ids) {
-//                    $query
-//                        ->fromRaw('categories nested_set_0, categories nested_set_1')
-//                        ->selectRaw('distinct nested_set_0.id')
-//                        ->whereIn('nested_set_1.id', $ids)
-//                        ->whereRaw('nested_set_0._lft between nested_set_1._lft and nested_set_1._rgt');
-//                };
-//
-//                $query->join(Tables::PRODUCT_CATEGORIES . ' as pc', function (JoinClause $join) use ($nestedSet) {
-//                    $join->on('pc.product_id', 'id')->whereIn('pc.category_id', $nestedSet);
-//                });
 
         //        $query->join(Tables::PRODUCT_CATEGORIES . ' as pc', function (JoinClause $join) use ($ids) {
         //            $join->on('pc.product_id', 'id')->whereIn('pc.category_id', $ids);
